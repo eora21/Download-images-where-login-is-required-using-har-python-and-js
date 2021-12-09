af1dd201719f0624d02bb69638a612603e64f155
eora21 <eora21@naver.com>
2021-12-09 15:16:59 +0900

    js modify

--- images_download.js
+++ images_download.js
@@ -1,17 +1,19 @@
-async function downloadImage(imageSrcs) {
-  const wait = (timeToDelay) => new Promise((resolve) => setTimeout(resolve, timeToDelay)) //이와 같이 선언 후
-  imageSrcs.forEach(async (imageSrc, idx) => {
-    
-    const image = await fetch(imageSrc)
-    const imageBlog = await image.blob()
-    const imageURL = URL.createObjectURL(imageBlog)
-    
-    const link = document.createElement('a')
-    link.href = imageURL
-    link.download = `${idx}.jpg`
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-    await wait(2000)    // 이렇게 await와 함께 사용하면 동기적으로 동작한다.
+function downloadImage(imageSrcs) {
+  imageSrcs.forEach((imageSrc, idx) => {
+    setTimeout(function (){
+
+      fetch(imageSrc)
+      .then(res => res.blob())
+      .then(imageBlog => {
+        const imageURL = URL.createObjectURL(imageBlog)
+        
+        const link = document.createElement('a')
+        link.href = imageURL
+        link.download = `${idx + 1}.jpg`
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+      })
+    }, idx * 500);
   });
 }
